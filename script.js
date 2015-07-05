4e3f35b768629b79f304a68bd1db6457f93de59a
Erin Mahoney <erinmahoneyphoto@gmail.com>
2015-07-05 09:56:26 -0700

    Added list template and seed data in default panels

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 $(function() {
 
+	// element to hold the list of piekus
+	var $pieList = $('#pieku-list-panel')
+	
+	// post template
+	var postTemplate = _.template($('#post-template').html());
+
+	// sample posts
+	var posts = [
+		{title:"For the love of pie 1" , author: "Erin Mahoney", post: "When I look at it" + "<br>" + "The circle becomes a slice" + "<br>" + "I eat the whole pie."},
+		{title:"For the love of pie 2" , author: "Erin Mahoney", post: "When I look at it" + "<br>" + "The circle becomes a slice" + "<br>" + "I eat the whole pie."},
+		{title:"For the love of pie 3" , author: "Erin Mahoney", post: "When I look at it" + "<br>" + "The circle becomes a slice" + "<br>" + "I eat the whole pie."}
+	];
+
+	// append existing posts (from posts) to $pieList.
+	// '_.each' is an "iterator" function provided by Underscore.js
+	_.each(posts, function(post, index) {
+		var $post = $(postTemplate(post));
+		$post.attr('data-index', index);
+		$pieList.append($post);
+	});
+
+
 
 
 	
